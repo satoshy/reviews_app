02e12a82dcc2a5477cd37324fb92d24b3c333500
satoshy <satoshy@ya.ru>
2017-06-04 21:01:35 +0300

    added admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get '', to: 'dashboard#index', as: '/'
+  end
+
   resources :videos
   resources :comments
   devise_for :users
   root 'home#index'
-
 end
